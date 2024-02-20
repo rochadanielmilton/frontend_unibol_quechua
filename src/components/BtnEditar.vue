627e6c9124dc99290f8adaa5698adb91d89dfeab
johnny-aras <89922953+johnny-aras@users.noreply.github.com>
2024-02-20 17:15:53 -0400

    lc

--- src/components/BtnEditar.vue
+++ src/components/BtnEditar.vue
@@ -2,8 +2,7 @@
     <div>                
       <button class="btn btn-warning"  @click="Editar">
         Editar
-      </button><i class="fa-solid fa-user"></i>
-        
+      </button><i class="fa-solid fa-user"></i>        
     </div>
   </template>
   
@@ -11,16 +10,22 @@
   //import UploadService from "../services/UploadFilesService";
   
   export default {
+    props: ['estudiantes'],
     name: "BtnEditar",
     data() {
       return {
         
       };
     },
+    setup(props) {
+    // setup() receives props as the first argument.
+    console.log(props.estudiantes)
+    },
     methods: {
       Editar()
       {
         console.log('hsata aki todo positivo');
+        console.log(props.estudiantes);
       }   
     },
     mounted() {
